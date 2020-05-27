81bb5d942e928299c7d9141484a56e0792ec4ad4
laurenmaher13@hotmail.com <laurenmaher13@hotmail.com>
2020-05-27 15:35:26 +0100

    kitchen

--- Assets/Scripts/Show-Hide/Finish.cs
+++ Assets/Scripts/Show-Hide/Finish.cs
@@ -4,7 +4,7 @@ public class Finish : MonoBehaviour
 {
    
     public GameObject Finished;
-    public GameObject Finished2;
+ 
 
 
     void OnMouseDown()
@@ -12,7 +12,6 @@ public class Finish : MonoBehaviour
       
             Finished.SetActive(true);
 
-        Finished2.SetActive(true);
 
     }
 }
