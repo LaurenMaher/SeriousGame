c6736b82e0f14ab7dcb8db124458578730926cb7
laurenmaher13@hotmail.com <laurenmaher13@hotmail.com>
2020-06-17 23:51:14 +0100

    working avatars

--- Assets/Scenes/Levels_Woman/Scripts/ChangeScenes/MainMenu/PlayGame.cs
+++ Assets/Scenes/Levels_Woman/Scripts/ChangeScenes/MainMenu/PlayGame.cs
@@ -7,6 +7,6 @@ public class PlayGame : MonoBehaviour
 {
     void Start()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1Outside_"); 
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ChoosePlayer"); 
     }
 }
